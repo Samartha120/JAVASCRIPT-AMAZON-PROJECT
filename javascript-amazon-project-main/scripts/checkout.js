7e3e53570d3eb831f5be38ac72d40baff595c55c
Samartha@Shrestha <samartha.shrestha@gmail.com>
2025-10-29 02:51:58 +0530

    Modified the delivery option and making it interactive

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart,removeFromCart} from '../data/cart.js';
+import {cart,removeFromCart,updateDeliveryOption} from '../data/cart.js';
 import {products}  from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 import {deliveryOptions} from '../data/deliveryOption.js';
@@ -110,7 +110,9 @@ console.log(DeliveryDate.format('dddd ,  MMMM D'));
 
     const ischecked = deliveryOption.id === cartItem.deliveryOptionId;
  html+= `
-  <div class="delivery-option">
+  <div class="delivery-option  js-delivery-option">
+                data-product-id ="${matchingProduct.id}"
+                data-delivery-option-id = "${deliveryOption.id}">
                     <input type="radio"
                   ${ischecked ? 'checked' : ''}
                         class="delivery-option-input"
@@ -146,4 +148,13 @@ console.log(DeliveryDate.format('dddd ,  MMMM D'));
 
        containerToRemove.remove();
     })
+   })
+
+   document.querySelectorAll('.js-delivery-option')
+   .forEach((element)=>{
+    element.addEventListener('click',()={
+         const {productId,deliveryOptionId}=element.dataset;
+         updateDeliveryOption(productId,deliveryOptionId);
+    })
+
    })
